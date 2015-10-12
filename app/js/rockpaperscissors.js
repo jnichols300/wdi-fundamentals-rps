cee1922a165b590198282d76dadd62c8f941728b
Jared Nichols <jnichols300@gmail.com>
2015-10-11 23:36:18 -0400

    Update rockpaperscissors.js

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -42,27 +42,61 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    if (playerMove === 'rock' && computerMove === 'paper') { winner = 'computer';}
-    else if (playerMove === 'rock' && computerMove === 'scissors') {winner = 'player';}
-    else if (playerMove === 'rock' && computerMove === 'rock') {winner = 'tie';}
-    else if (playerMove === 'scissors' && computerMove === 'scissors') {winner = 'tie';}
-    else if (playerMove === 'scissors' && computerMove === 'rock') {winner = 'computer';}
-    else if (playerMove === 'scissors' && computerMove === 'paper') {winner = 'player';}
-    else if (playerMove === 'paper' && computerMove === 'scissors') {winner = 'computer';}
-    else if (playerMove === 'paper' && computerMove === 'rock') {winner = 'player';}
-    else if (playerMove === 'paper' && computerMove === 'paper') {winner = 'tie';}
-    else {
-        console.log ('You're wrong.');
+ if (playerMove == computerMove) {
+        winner = "tie";
+    }
+    else if (playerMove == "rock" && computerMove == "scissors") {
+        winner = 'player';
+    }
+    else if (playerMove == "scissors" && computerMove == "rock") {
+        winner = 'computer';
+    }
+    else if (playerMove == "scissors" && computerMove == "paper") {
+        winner = 'player';
+    }
+    else if (playerMove == "paper" && computerMove == "scissors") {
+        winner = 'computer';
+    }
+    else if (playerMove == "paper" && computerMove == "rock") {
+        winner = 'player';
+    }
+    else if (playerMove == "rock" && computerMove == "paper") {
+        winner = 'computer';
     }
     return winner;
 }
 
 function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
+ console.log("Let's play Rock Paper Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+    while (playerWins < 5 && computerWins < 5) {
+        var playerChose = getPlayerMove();
+        var computerChose = getComputerMove();
+        var roundWinner = getWinner(playerChose, computerChose);
+        if (roundWinner === 'player') {
+            playerWins +=  1;
+          	//these two console.log must be in each condition because if score is only 
+          	//updated after the condition is true.
+        	console.log('Player chose ' + playerChose + ' while Computer chose ' + computerChose);
+        	console.log('The score is currently ' + playerWins + ' to ' + computerWins);
+        } else if (roundWinner === 'computer') {
+            computerWins += 1;
+          	//these two console.log must be in each condition because if score is only 
+          	//updated after the condition is true.
+          	console.log('Player chose ' + playerChose + ' while Computer chose ' + computerChose);
+        	console.log('The score is currently ' + playerWins + ' to ' + computerWins);
+        } 
+    }
+  	//you need to return something, so that when you call the function, it return something
+  	//we can ignore [playerWins, computerWins] since we already print it about for each game
+  	//cuz the pre-setup function have it, so I/we should keep it.
+    if (playerWins === 5) {
+        return "You win! " + [playerWins, computerWins];
+    } else if (computerWins === 5) {
+        return "You lose, try again. " + [playerWins, computerWins];
+    }  
 }
+
+console.log(playToFive());
 
